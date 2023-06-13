50e2b4aeabc2e18b46eb38e08cfb586786753f9d
Collins <collinskiarie13@outlook.com>
2023-06-13 13:13:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Kiarie Kariuki";
+const excitedCurrentUser = "KIARIE KARIUKI";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+
+const excitedWelcomeMessage = " WELCOME TO FLATBOOK, "  + excitedCurrentUser + "!";  
+
+const shortGreeting = `Welcome, ${currentUser.slice (0,1)}!`;
+
+
+
+
